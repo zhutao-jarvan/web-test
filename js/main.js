bd371024dabe760002679d8685f4f79c8d92f339
zhutao-jarvan <583043362@qq.com>
2018-03-04 21:51:56 +0800

    add

--- js/main.js
+++ js/main.js
@@ -1,35 +1,44 @@
 window.onload = function () {
-    body_content("m_main");
+    //body_content("m_main");
 }
 
 function body_content(menu_select) {
-    document.getElementById("banner_main").hidden = true;
-    document.getElementById("login").hidden = true;
-    document.getElementById("org_story").hidden = true;
-    document.getElementById("hot_topic").hidden = true;
-    document.getElementById("beautiful_art").hidden = true;
-    document.getElementById("support").hidden = true;
+    var vals = new Array("hidden", "hidden", "hidden", "hidden", "hidden", "hidden");
     switch (menu_select) {
         case "m_main":
-            document.getElementById("banner_main").hidden = false;
+            //document.getElementById("banner_main").hidden = false;
+            vals[0] = "visible";
             break;
         case "m_org_story":
-            document.getElementById("org_story").hidden = false;
+            //document.getElementById("org_story").hidden = false;
+            vals[2] = "visible";
             break;
         case "m_hot_topic":
-            document.getElementById("hot_topic").hidden = false;
+            //document.getElementById("hot_topic").hidden = false;
+            vals[3] = "visible";
             break;
         case "m_beautiful_art":
-            document.getElementById("beautiful_art").hidden = false;
+            //document.getElementById("beautiful_art").hidden = false;
+            vals[4] = "visible";
             break;
         case "m_support":
-            document.getElementById("support").hidden = false;
+            //document.getElementById("support").hidden = false;
+            vals[5] = "visible";
             break;
         case "login_btn":
-            document.getElementById("login").hidden = false;
+            //document.getElementById("login").hidden = false;
+            vals[1] = "visible";
             break;
         default:
-            document.getElementById("banner_main").hidden = false;
+            //document.getElementById("banner_main").hidden = false;
+            vals[0] = "visible";
             break;
     }
+
+    document.getElementById("banner_main").style.visibility = vals[0]; //0
+    document.getElementById("login").style.visibility = vals[1];      //1
+    document.getElementById("org_story").style.visibility = vals[2];  //2
+    document.getElementById("hot_topic").style.visibility = vals[3];  //3
+    document.getElementById("beautiful_art").style.visibility = vals[4]; //4
+    document.getElementById("support").style.visibility = vals[5];   //5
 }
